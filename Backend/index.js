c67cac81a836277bc3c71c93ef2e315a728ec34e
Ritik Balbudhe <balbudheritik155@gmail.com>
2026-09-25 22:33:30 +0530

    Update URL

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app=express();
 connectDB();
 
-app.use(cors({origin:process.env.FRONTEND_URL||"http://localhost:5173",credentials:true}));
+app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}));
 app.use(express.json({ limit: "3mb" }));
 app.use("/api/auth",authRouter);
 app.use("/api/cart",cartRouter);
